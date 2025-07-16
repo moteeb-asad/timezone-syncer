0955d40766ab099ac51758fd88e675624b7ae235
moteeb-asad <moteeb83@gmail.com>
2025-07-16 11:29:15 +0500

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./App.tsx";
+import "./index.css";
+import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store.ts";
+import store from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
